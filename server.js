f169e2235baaf7d2851bbf69afe625981446bcb9
kamalkumar1153- <kamalkumar1153@gmail.com>
2026-04-15 14:50:46 +0530

    Fix databaseURL syntax in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (!admin.apps.length) {
             // Ye key Render dashboard ke Environment Variables se aayegi
             privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : ""
         }),
-        databaseURL: "https://ai-pro-terminal-default-rtdb.firebaseio.com"/
+        databaseURL: https://ai-pro-terminal-default-rtdb.firebaseio.com/
     });
 }
 const db = admin.database();
